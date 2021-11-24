8ba186600ce36f6bbc6daa7aa02c642184a44348
Jan Mikhail Talanay <12004259@benilde.edu.ph>
2021-11-24 17:32:14 +0800

    FIX

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ using Farmerce.Models;
 
 namespace Farmerce.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
