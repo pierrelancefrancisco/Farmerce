ed3ab9b65cfec89988b78d1bd7320f0ad06fb453
Jan Mikhail Talanay <12004259@benilde.edu.ph>
2021-11-25 22:06:42 +0800

    Admin

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace Farmerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            if(!string.IsNullOrEmpty(email) && string.IsNullOrEmpty(password))
+            {
+                return RedirectToAction("Login");
+            }
+            ClaimsIdentity identity = null;
+            bool isAuthenitcate = false;
+            if (email == "Testing@gmail.com" && password == "A")
+            {
+                identity = new ClaimsIdentity(new [])
+            }
+            return View();
+        }
+    }
+}
